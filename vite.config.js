281d68614fe12e84078f60d076578d925793e1a8
grufre349 <grufre349@gmail.com>
2025-09-22 11:51:24 +0200

    try to deploy with Github Actions while simply following steps from LMS 1:1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,20 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: "/CI/",
 	plugins: [react(), tailwindcss()],
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: ["./src/vitest.setup.ts"],
+		coverage: {
+			provider: "istanbul", // Istanbul JS is a test coverage tool
+			reporter: ["text", "json", "html"],
+			thresholds: {
+				branches: 80,
+				functions: 80,
+				statements: 80,
+			},
+		},
+	},
 });
