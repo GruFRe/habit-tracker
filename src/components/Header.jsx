d5f7b6897ca1bb6248376b4d71b07c4601ca1fc3
grufre349 <grufre349@gmail.com>
2025-09-22 12:00:50 +0200

    altered Headline again

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export const Header = () => {
 	return (
 		<header>
 			<h1 className="bg-green-600 text-center py-2">
-				Habit Tracker with Github Actions
+				Habit Tracker with Github Actions v1
 			</h1>
 		</header>
 	);
